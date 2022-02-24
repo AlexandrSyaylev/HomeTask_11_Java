d38bb0c499b75640e62979d11a14eef6f422207a
Alexander Syayalev <alexxs@inbox.ru>
2022-02-24 19:47:01 +0300

    remove creation manager to class flied

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -11,13 +11,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProductManagerTest {
 
     ProductRepository repo = new ProductRepository();
+    ProductManager manager = new ProductManager(repo);
     Book first = new Book(1, "A4", 2000, "Pushkin");
     Book second = new Book(1, "A5", 10000, "Pushkin");
     Smartphone firstSmartphone = new Smartphone(1, "A5", 10000, "samsung");
 
     @Test
     public void shouldAddOneBookUseRepoUseManager() {
-        ProductManager manager = new ProductManager(repo);
         manager.add(first);
         int expected = 1;
         int actual = repo.findALL().length;
@@ -26,7 +26,6 @@ class ProductManagerTest {
 
     @Test
     public void shouldAddOneSmartphoneToRepoUseManager() {
-        ProductManager manager = new ProductManager(repo);
         manager.add(firstSmartphone);
         int expected = 1;
         int actual = repo.findALL().length;
@@ -35,7 +34,6 @@ class ProductManagerTest {
 
     @Test
     public void shouldAddOneSmartphoneAndOneBookToRepoUseManager() {
-        ProductManager manager = new ProductManager(repo);
         manager.add(firstSmartphone);
         manager.add(second);
         int expected = 2;
@@ -45,7 +43,6 @@ class ProductManagerTest {
 
     @Test
     public void shouldNotSearchOneBookInRepoUseManager() {
-        ProductManager manager = new ProductManager(repo);
         manager.add(second);
         int actual = manager.searchBy("B").length;
         int expected = 0;
@@ -54,7 +51,6 @@ class ProductManagerTest {
 
     @Test
     public void shouldSearchOneBookInRepoUseManager() {
-        ProductManager manager = new ProductManager(repo);
         manager.add((first));
         manager.add(second);
         int actual = manager.searchBy("A5").length;
